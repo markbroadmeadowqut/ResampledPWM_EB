c272ae26cc1b158b8705271b15974e7c699215f2
edward-burstinghaus <edward.burstinghaus@qut.edu.au>
2017-02-08 16:18:06 -0500

    Erroneous calculation of calc1 for ePWM2 and ePWM3 was corrected; PWM glitches that can be identified with the naked eye appear to have been removed for f0 up to 1kHz; phase delay improvement of FOH over ZOH appears to be there but very difficult to confirm on oscilloscope.

--- F28027_ResampledPWM_FOH/config.h
+++ F28027_ResampledPWM_FOH/config.h
@@ -15,4 +15,5 @@
 #define SWTBPRD (Uint16)4095  // Guarantees maximum possible accuracy from the 12-bit ADC; shifts the carrier frequency to 7327 Hz (=TBCLK/(2*TBPRD))
 #define SWTBPHS23 (Uint16)2730  // =2*SWTBPRD/3; phase offset for both ePWM2 and ePWM3
 #define FOH_SCALE (Uint16)3000 // =TBCLK/FS; both in Hz, TBCLK=60 MHz and FS corresponding to a 50us ADC conversion time
+#define FOH_SCALE_doubled (Uint16)6000 // =TBCLK/FS; both in Hz, TBCLK=60 MHz and FS corresponding to a 50us ADC conversion time
 #endif /* CONFIG_H_ */

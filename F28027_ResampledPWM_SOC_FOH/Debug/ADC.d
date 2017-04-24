8955f3a07b17a685d19b579ce78e8b03fc9629b6
edward-burstinghaus <edward.burstinghaus@qut.edu.au>
2017-04-25 08:49:41 +1000

    Second Order Correction to the level of the FOH (i.e. vertical translation by half of the error that was produced by the normal FOH at the most recent sample-update)

---
+++ F28027_ResampledPWM_SOC_FOH/Debug/ADC.d
@@ -0,0 +1,53 @@
+# FIXED
+
+ADC.obj: ../ADC.c
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP28x_Project.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Device.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Adc.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_BootVars.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_DevEmu.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Comp.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_CpuTimers.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_ECap.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_EPwm.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Gpio.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_I2c.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_NmiIntrupt.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_PieCtrl.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_PieVect.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Spi.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Sci.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_SysCtrl.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_XIntrupt.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_Examples.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_GlobalPrototypes.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_EPwm_defines.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_I2C_defines.h
+ADC.obj: C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_DefaultISR.h
+ADC.obj: ../config.h
+
+../ADC.c: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP28x_Project.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Device.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Adc.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_BootVars.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_DevEmu.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Comp.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_CpuTimers.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_ECap.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_EPwm.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Gpio.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_I2c.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_NmiIntrupt.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_PieCtrl.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_PieVect.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Spi.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_Sci.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_SysCtrl.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_headers/include/DSP2802x_XIntrupt.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_Examples.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_GlobalPrototypes.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_EPwm_defines.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_I2C_defines.h: 
+C:/Git/ResampledPWM_EB/F28027_ResampledPWM_SOC_FOH/DSP2802x_common/include/DSP2802x_DefaultISR.h: 
+../config.h: 

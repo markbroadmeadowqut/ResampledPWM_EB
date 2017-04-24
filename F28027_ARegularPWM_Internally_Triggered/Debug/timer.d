0d3f81bae07fafe621248bce882f5b04405640aa
edward-burstinghaus <edward.burstinghaus@qut.edu.au>
2017-04-25 08:42:20 +1000

    Asymmetrical regular PWM for the F28027 with ADC-SOC triggered seperately by each ePWM module at peak and trough events

---
+++ F28027_ARegularPWM_Internally_Triggered/Debug/timer.d
@@ -0,0 +1,53 @@
+# FIXED
+
+timer.obj: ../timer.c
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP28x_Project.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Device.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Adc.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_BootVars.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_DevEmu.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Comp.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_CpuTimers.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_ECap.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_EPwm.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Gpio.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_I2c.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_NmiIntrupt.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_PieCtrl.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_PieVect.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Spi.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Sci.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_SysCtrl.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_XIntrupt.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_Examples.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_GlobalPrototypes.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_EPwm_defines.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_I2C_defines.h
+timer.obj: C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_DefaultISR.h
+timer.obj: ../config.h
+
+../timer.c: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP28x_Project.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Device.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Adc.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_BootVars.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_DevEmu.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Comp.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_CpuTimers.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_ECap.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_EPwm.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Gpio.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_I2c.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_NmiIntrupt.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_PieCtrl.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_PieVect.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Spi.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_Sci.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_SysCtrl.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_headers/include/DSP2802x_XIntrupt.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_Examples.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_GlobalPrototypes.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_EPwm_defines.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_I2C_defines.h: 
+C:/Git/ResampledPWM_EB/F28027_ARegularPWM_Internally_Triggered/DSP2802x_common/include/DSP2802x_DefaultISR.h: 
+../config.h: 
